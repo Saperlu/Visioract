8d05d7e54a056ed9ff093d0dec48456c2b466db3
Saperlu <saperli000@gmail.com>
2021-05-24 20:23:42 +0200

    Clock

--- functions.js
+++ functions.js
@@ -1,8 +1,14 @@
 let data = {};
 let seenScenes = {};
+
+let clockBox = document.getElementById("clockBox");
 let choiceBox = document.getElementById("choiceBox");
 let videoBox = document.getElementById("videoBox");
 let textBox = document.getElementById("textBox");
+let timeLeft = new Date(2000, 0, 1, 0, 20);
+let timeEnd = new Date(2000, 0, 1);
+let isTimeToSet = false;
+let previousSceneId = "";
 let currentSceneId = "";
 let currentSceneType;
 
@@ -37,7 +43,8 @@ function setupScene(sceneId) {
     clearSetup();
     currentSceneId = sceneId;
     currentSceneType = scene.type;
-
+    if (scene.time || scene.time === 0)
+        clockBox.className = "displayed";
     // Content
     switch (scene.type) {
         case "text": 
@@ -50,7 +57,6 @@ function setupScene(sceneId) {
             videoBox.insertBefore(textBox, choiceBox);
             break;
             case "video":
-                // TODO : video case
                 video = document.createElement("video");
                 video.src = `ressources/videos/${sceneId}.mp4`;
                 video.type = "video/mp4";
@@ -62,28 +68,44 @@ function setupScene(sceneId) {
                     video.style.height = targetHeight;
 
                     choiceBox.style.opacity = 1;
+                    if (scene.time)
+                        setTimeLeft(scene.time);
+
+                        
                 };
                 videoBox.insertBefore(video, choiceBox);
                 video.style.height = video.clientHeight + "px";
             break;
             case "menu video":
-                // TODO : video case
                 video = document.createElement("video");
-                video.src = `ressources/videos/${sceneId}.mp4`;
-                video.type = "video/mp4";
-                video.controls = false;
-                video.autoplay = true;
-                video.onplay = (ev) => {
-                    choiceBox.style.display = "flex";
-                    let targetHeight = window.innerHeight - choiceBox.clientHeight + "px";
-                    video.style.height = targetHeight;
-                    choiceBox.style.opacity = 1;
-                };
-
+                if (isTimeToSet && timeLeft - data.scenes[previousSceneId].time*1000 <= timeEnd) {
+                    isTimeToSet = false;
+                    video.style.display="none";
+                    setTimeLeft(data.scenes[previousSceneId].time);
+                } else {
+                    video.src = `ressources/videos/${sceneId}.mp4`;
+                    video.type = "video/mp4";
+                    video.controls = false;
+                    video.autoplay = true;
+                    video.onplay = (ev) => {
+                        if (isTimeToSet){
+                            isTimeToSet = false;
+                            setTimeLeft(data.scenes[previousSceneId].time);
+                        }
+                        choiceBox.style.display = "flex";
+                        let targetHeight = window.innerHeight - choiceBox.clientHeight + "px";
+                        video.style.height = targetHeight;
+                        choiceBox.style.opacity = 1;
+                        if (scene.time)
+                        setTimeLeft(scene.time);
+                    };
+    
+                    video.style.height = video.clientHeight + "px";
+                }
                 videoBox.insertBefore(video, choiceBox);
-                video.style.height = video.clientHeight + "px";
             break;
-        default:
+        default: 
+            console.error(scene.type);
             break;
     }
 
@@ -99,6 +121,9 @@ function setupScene(sceneId) {
             choiceBox.appendChild(div);
             if (choice.autoChoice && currentSceneType === "video") {
                 video.onended = () => {
+                    previousSceneId = currentSceneId;
+                    if (scene.time)
+                        isTimeToSet = true;
                     handleChoice(choice.sceneId);
                 }
             }
@@ -108,6 +133,7 @@ function setupScene(sceneId) {
 
 
 function clearSetup() {
+    clockBox.className = "";
     choiceBox.style.display = "none";
     choiceBox.style.transitionProperty = "none";
     choiceBox.style.opacity = 0;
@@ -158,4 +184,72 @@ function clearNode(node) {
     while (node.firstChild) {
         node.removeChild(node.firstChild);
     }
-}   
+}
+
+function setTimeLeft(timeTaken) {
+    timeTaken *= 1000; // From milliseconds to seconds
+
+    newTimeLeft = new Date(timeLeft - timeTaken);
+    if (newTimeLeft <= timeEnd)
+        newTimeLeft = timeEnd;
+
+
+    for (let i = 1; i < 10; i++) {
+        setTimeout(() => {
+            if (getDizaines(timeLeft.getMinutes()) === getDizaines(newTimeLeft.getMinutes())) {
+                if (getUnites(timeLeft.getMinutes()) === getUnites(newTimeLeft.getMinutes())) {
+                    if (getDizaines(timeLeft.getSeconds()) === getDizaines(newTimeLeft.getSeconds())) {
+                        if (getUnites(timeLeft.getMinutes()) === getUnites(newTimeLeft.getSeconds())) {
+                            // On ne fait rien si le temps ne bouge pas
+                        } else {
+                            USecondes = randomDigit();
+                        }   
+                    } else {
+                        DSecondes = randomDigit();
+                        USecondes = randomDigit();
+                    }
+                } else {
+                    UMinutes = randomDigit();
+                    DSecondes = randomDigit();
+                    USecondes = randomDigit();
+                }
+            } else {
+                DMinutes = randomDigit();
+                UMinutes = randomDigit();
+                DSecondes = randomDigit();
+                USecondes = randomDigit();
+            }
+            chaine = `${DMinutes}${UMinutes}:${DSecondes}${USecondes}`
+            clockBox.textContent = chaine;
+        }, i*100);
+    }
+
+    setTimeout(() => {
+        timeLeft = newTimeLeft;
+        DMinutes = getDizaines(timeLeft.getMinutes());
+        UMinutes = getUnites(timeLeft.getMinutes());
+        DSecondes = getDizaines(timeLeft.getSeconds());
+        USecondes = getUnites(timeLeft.getSeconds());
+        chaine = `${DMinutes}${UMinutes}:${DSecondes}${USecondes}`
+        clockBox.textContent = chaine;
+        if (timeLeft === timeEnd)
+        {
+            clockBox.className = "displayed animated";
+            // TODO : Remplacer par accuser les suspects
+            setTimeout(() => {
+                handleChoice("pKn_alibi")
+            }, 2500);
+        }
+    }, 1020);
+}
+
+function randomDigit () {
+    return Number.parseInt(Math.random()*10);
+} 
+function getDizaines (n) {
+    return Number.parseInt(n/10);
+}
+
+function getUnites (n) {
+    return n%10;
+}
